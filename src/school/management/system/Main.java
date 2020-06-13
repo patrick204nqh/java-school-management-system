fa9e1d83ab7191193a4777bd6601d7956f985b6b
patrick <patrick204nqh@gmail.com>
2020-06-13 10:53:24 +0700

    set data(students & teachers)

--- src/school/management/system/Main.java
+++ src/school/management/system/Main.java
@@ -5,7 +5,26 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        Teacher lizzy = new Teacher(1,"Lizzy", 500);
+        Teacher mellisa = new Teacher(2, "Mellisa", 700);
+        Teacher vanderhorn = new Teacher(3, "vanderhorn", 600);
 
+        List<Teacher> teacherList = new ArrayList<>();
+        teacherList.add(lizzy);
+        teacherList.add(mellisa);
+        teacherList.add(vanderhorn);
+
+        Student patrick = new Student(1, "Patrick", 12);
+        Student robert = new Student(2, "Robert", 12);
+        Student kevin = new Student(3, "Kevin", 10);
+
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(patrick);
+        studentList.add(robert);
+        studentList.add(kevin);
+
+        School ghs = new School(teacherList, studentList);
+        System.out.println("GHS has earned " + ghs.getTotalMoneyEarned());
 
 
     }
